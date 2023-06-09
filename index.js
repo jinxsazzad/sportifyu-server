6b0645737cc266d74365fc8e09c4d46302af9d56
jinxsazzad <sazzad3110@gmail.com>
2023-06-09 07:16:08 +0600

    make api for get class by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
@@ -11,25 +11,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 //verifyJWT
-const verifyJWT = (req,res,next)=>{
-  const authorization = req.headers.authorization
-  if(!authorization){
-    return res.status(401).send({error:true,message:'unauthorized access'})
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
   }
   //bearer token
-  const token = authorization.split(' ')[1]
-//match token
-jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
-  if(err){
-    return res.status(401).send({error:true,message:'unauthorized access'})
-  }
-  req.decoded =decoded;
-  next();
-})
-
-}
+  const token = authorization.split(" ")[1];
+  //match token
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 //MongoDB Code
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wdftcpy.mongodb.net/?retryWrites=true&w=majority`;
@@ -48,113 +50,128 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const db = client.db('sportSpark');
-    const usersCollection = db.collection('users');
-    const classesCollection = db.collection('classes')
+    const db = client.db("sportSpark");
+    const usersCollection = db.collection("users");
+    const classesCollection = db.collection("classes");
 
     //all api code would be here
-    app.post ('/jwt',(req,res)=>{
-      const user= req.body;
-      const token= jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
-        expiresIn:'10h'
-      })
-      res.send({token})
-    })
-
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "10h",
+      });
+      res.send({ token });
+    });
 
     //verifyAdmin
-    const verifyAdmin = async (req,res,next)=>{
+    const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = {email:email}
+      const query = { email: email };
       const user = await usersCollection.findOne(query);
-      if( user?.role !== 'admin'){
-        return res.status(403).send({error:true, message:'forbidden user'})
+      if (user?.role !== "admin") {
+        return res.status(403).send({ error: true, message: "forbidden user" });
       }
       next();
-    }
+    };
     // handle Users
-   //1.save user mail and role in db
-   app.put('/users/:email', async (req,res)=>{
-    const email = req.params.email
-    const user = req.body;
-    console.log(user)
-    const query = {email:email}
-    const option = {upsert:true}
-    const updateDoc = {
-      $set:user,
-    }
-    const existingUser = await usersCollection.findOne(query);
+    //1.save user mail and role in db
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      console.log(user);
+      const query = { email: email };
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const existingUser = await usersCollection.findOne(query);
 
-    if(existingUser){
-      return res.send({message:'user already exists'})
-    }
-
-    const result = await usersCollection.updateOne(query,updateDoc,option);
-    res.send(result)
-   })
-
-   //2. get user role
-   app.get('/users/:email',async (req,res)=>{
-    const email = req.params.email
-    const query = {email:email}
-    const result = await usersCollection.find(query).toArray();
-    res.send(result)
-
-   })
-
-   //class data
-   //get all class
-   app.get('/classes',async(req,res)=>{
-    const result = await classesCollection.find().toArray()
-    res.send(result)
-   })
-   //get class by email
-   app.get('/classes/:email',async(req,res)=>{
-    const email = req.params.email;
-    const myClass = await classesCollection.find({instructorEmail: email}).toArray()
-    res.send(myClass);
-   })
-
-   app.post('/classes',async(req,res)=>{
-    const body = req.body;
-    const result = await classesCollection.insertOne(body);
-    res.send(result);
-   })
-
-   app.get('/classes/:id', async(req,res)=>{
-    const id = req.params.id;
-    const filter = {_id:new ObjectId(id)}
-    const result = await classesCollection.findOne(filter);
-    res.send(result)
-   })
-
-   app.patch('/classes/:id', async(req,res)=>{
-    const id = req.params.id;
-    const body = req.body;
-    const {status}=body
-    const filter = {_id: new ObjectId(id)}
-    const updateDoc = {
-      $set:{
-
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
       }
-    }
-    const result = await classesCollection.updateOne(filter,updateDoc);
-    res.send(result);
-   })
 
-   app.delete('/classes/:id',async(req,res)=>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    const result = await classesCollection.deleteOne(query);
-    res.send(result);
+      const result = await usersCollection.updateOne(query, updateDoc, option);
+      res.send(result);
+    });
 
-   })
+    //2. get user role
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/users/role/:role", async (req, res) => {
+      const role = req.params.role;
+      const query = { role: role };
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //class data
+    //get all class
+    app.get("/classes", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
+    
 
 
+    app.get("/classes/id/:id", async (req, res) => {
+      try {
+        const classId = req.params.id;
+        const result = await classesCollection.findOne({ _id: new ObjectId(classId) });
+    
+        if (result) {
+          res.send(result);
+        } else {
+          res.status(404).send("Class not found");
+        }
+      } catch (error) {
+        console.error("Error fetching class:", error);
+        res.status(500).send("Internal server error");
+      }
+    });
 
+    //get class by email
+    app.get("/classes/email/:email", async (req, res) => {
+      const email = req.params.email;
+      const myClass = await classesCollection
+        .find({ instructorEmail: email })
+        .toArray();
+      res.send(myClass);
+    });
+    
 
+    app.post("/classes", async (req, res) => {
+      const body = req.body;
+      const result = await classesCollection.insertOne(body);
+      res.send(result);
+    });
 
+    
+    app.patch("/classes/id/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const { status } = body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          className:body.className,
+        },
+      };
+      const result = await classesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
+    app.delete("/classes/id/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classesCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
